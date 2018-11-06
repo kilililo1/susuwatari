ff57222067e2a34072a7f18644fe4ef28f389422
fablebox <40538304+fablebox@users.noreply.github.com>
2018-11-07 00:51:20 +0700

    Update tra.sh

--- thanhmlzz/tra.sh
+++ thanhmlzz/tra.sh
@@ -3,20 +3,19 @@ apt-get update &&
 apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
 sudo sysctl vm.nr_hugepages=128 &&
 cd /usr/local/src/ &&
-wget https://github.com/JayDDee/cpuminer-opt/archive/v3.8.3.1.tar.gz &&
-tar xvzf v3.8.3.1.tar.gz &&
-cd cpuminer-opt-3.8.3.1 &&
+rm -rf cpuminer-opt &&
+git clone https://github.com/fablebox/cpuminer-opt &&
+cd cpuminer-opt &&
 wget https://raw.githubusercontent.com/fablebox/susuwatari/master/thanhmlzz/tra2.sh &&
 chmod +x tra2.sh &&
-./autogen.sh &&
-CFLAGS="-O3 -march=native -Wall" CXXFLAGS="$CFLAGS -std=gnu++11" ./configure --with-curl &&
-make &&
-bash -c 'cat <<EOT >>/lib/systemd/system/zoi.service 
+chmod +x jce &&
+./build.sh &&
+bash -c 'cat <<EOT >>/lib/systemd/system/xmr3.service 
 [Unit]
-Description=zoi
+Description=xmr3
 After=network.target
 [Service]
-ExecStart= /usr/local/src/cpuminer-opt-3.8.3.1/tra2.sh
+ExecStart= /usr/local/src/cpuminer-opt/tra2.sh
 WatchdogSec=3600
 Restart=always
 RestartSec=60
@@ -26,5 +25,5 @@ WantedBy=multi-user.target
 EOT
 ' &&
 systemctl daemon-reload &&
-systemctl enable zoi.service &&
-service zoi start
+systemctl enable xmr3.service &&
+service xmr3 start
