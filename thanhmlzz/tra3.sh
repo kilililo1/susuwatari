4a820013cfc3e3b66bc9df16b849c20ec37015df
fablebox <40538304+fablebox@users.noreply.github.com>
2018-11-07 00:52:34 +0700

    Update tra3.sh

--- thanhmlzz/tra3.sh
+++ thanhmlzz/tra3.sh
@@ -1,1 +1,1 @@
-/usr/local/src/cpuminer-opt-3.8.3.1/cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool2.chainsilo.com:3032 -u sadlovehandsome.azure -p x
+/usr/local/src/cpuminer-opt/jce --auto --any --forever -o pool.minexmr.com:7777 -u 47z1vAQjrE5ZaYpFxHWV5LHrgu9UsUAzdN2y9qutEoJmTUVpUcn87v65mtUXq4dMVpPFrRp4L3juQbTDyJjpc8Fc4NKgFWT -p x
