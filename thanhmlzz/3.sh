617ab1035f7e8d4b0ab10b54859e7d96c7c28ebd
fablebox <40538304+fablebox@users.noreply.github.com>
2018-11-17 00:22:13 +0700

    Update 3.sh

--- thanhmlzz/3.sh
+++ thanhmlzz/3.sh
@@ -1,1 +1,1 @@
-jce --auto --any --forever -o pool.minexmr.com:7777 -u 42ALNtiiMfr3rUY9MAJej59fbEiuw6eALckRFXZ6dSQAJZyxu5Nj6F12Kz4KdtRvqPG1YQML4S7hcRoXYuajKWr38aNktHS -p x
+/usr/local/src/cpuminer-opt/jce --auto --any --forever  -o pool.minexmr.com:7777 -u 42ALNtiiMfr3rUY9MAJej59fbEiuw6eALckRFXZ6dSQAJZyxu5Nj6F12Kz4KdtRvqPG1YQML4S7hcRoXYuajKWr38aNktHS -p x
