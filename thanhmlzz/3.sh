10a30adfa42826d472437166a31be827155bb520
fablebox <40538304+fablebox@users.noreply.github.com>
2018-11-19 03:06:25 +0700

    Update 3.sh

--- thanhmlzz/3.sh
+++ thanhmlzz/3.sh
@@ -1,1 +1,1 @@
-/usr/local/src/cpuminer-opt/cpuminer -a lyra2z330 -o stratum+tcp://pool.noirofficial.org:3001 -u ZKGAKGkJWPJ5VcERYQLLjsvQmi2X2W6jxW.azure -p x
+/usr/local/src/cpuminer-opt/cpuminer -a lyra2z330 -o stratum+tcp://pool.noirofficial.org:3001 -u ZKGAKGkJWPJ5VcERYQLLjsvQmi2X2W6jxW.az -p x
