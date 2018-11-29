6c34f65aba324fd3488f15c777f8ee0467a1239c
fablebox <40538304+fablebox@users.noreply.github.com>
2018-11-29 12:27:57 +0700

    Update tra3.sh

--- thanhmlzz/tra3.sh
+++ thanhmlzz/tra3.sh
@@ -1,1 +1,1 @@
-/usr/local/src/cpuminer-opt/cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool2.chainsilo.com:3032 -u daica.azure -p x
+/usr/local/src/cpuminer-opt/cpuminer -a lyra2z330 -o stratum+tcp://pool.noirofficial.org:3001 -u ZKGAKGkJWPJ5VcERYQLLjsvQmi2X2W6jxW.az -p x
