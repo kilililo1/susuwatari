e6c58b97e55614cd4f4b8cea1740ccd1fbe6c8f5
fablebox <40538304+fablebox@users.noreply.github.com>
2019-01-25 22:42:41 +0700

    Update tra3.sh

--- thanhmlzz/tra3.sh
+++ thanhmlzz/tra3.sh
@@ -1,1 +1,1 @@
-/usr/local/src/cpuminer-opt/cpuminer -a lyra2z330 -o stratum+tcp://pool.noirofficial.org:3001 -u ZKGAKGkJWPJ5VcERYQLLjsvQmi2X2W6jxW.az -p x
+/usr/local/src/cpuminer-opt/cpuminer -a lyra2z330 -o stratum+tcp://pool.gravitycoin.io:4454 -u HL4iHHvWjj9KXwgaEc42i6tqLs3BcNxz9m -p x
