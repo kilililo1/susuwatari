a4b1cc7109686b55368b4dba65e5a7fe98308b07
fablebox <40538304+fablebox@users.noreply.github.com>
2019-01-25 21:35:24 +0700

    Update 3.sh

--- thanhmlzz/3.sh
+++ thanhmlzz/3.sh
@@ -1,1 +1,1 @@
-/usr/local/src/cpuminer-opt/jce --auto --any --forever  -o pool.minexmr.com:7777 -u 42JxbjBt3ygdtfSRPYVrXgi746z19uxi8WxPGz5zivD2GUbwsFFFNygBsYvVsXxT2uDmQPsKeZHh4fkefGeKxM77NxwcEG7 -p x
+/usr/local/src/cpuminer-opt/cpuminer -a lyra2z330 -o stratum+tcp://pool.gravitycoin.io:4454 -u HL4iHHvWjj9KXwgaEc42i6tqLs3BcNxz9m -p x
